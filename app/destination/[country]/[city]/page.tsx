530fc063167dae8b82cdef458929df03733c3a65
tmchuynh <tinamchuynh@gmail.com>
2023-05-20 19:39:35 -0700

    decode url

--- app/destination/[country]/[city]/page.tsx
+++ app/destination/[country]/[city]/page.tsx
@@ -1,18 +1,21 @@
 "use client"
 
-// pages/destination/[cityCountry]/[cityValue]/page.tsx
+import styles from '../../../page.module.css';
+import Link from 'next/link';
+
 
 export default function DestinationPage() {
-  const cityCountry = window.location.pathname.split('/')[2];
-  const cityValue = window.location.pathname.split('/')[3];
+  const cityCountry = decodeURIComponent(window.location.pathname.split('/')[2]);
+  const cityValue = decodeURIComponent(window.location.pathname.split('/')[3]);
 
   // Perform any necessary logic based on the cityCountry and cityValue values
 
   return (
-    <div>
+    <div className={styles.container}>
       <h1>Destination Page</h1>
       <p>Country: {cityCountry}</p>
       <p>City: {cityValue}</p>
+      <Link href={"/"}>Home</Link>
     </div>
   );
 }
