184692b52b8abafb39bf8a7bcf94549d5345dd80
tmchuynh <tinamchuynh@gmail.com>
2023-05-20 19:40:11 -0700

    update

--- app/components/autocomplete.tsx
+++ app/components/autocomplete.tsx
@@ -3,57 +3,67 @@ import styles from '../page.module.css';
 import { Autocomplete } from '@mantine/core';
 
 interface City {
-    label: string;
-    value: string;
-    country: string;
+  label: string;
+  value: string;
+  country: string;
 }
 
 export default function AutoComplete() {
-    const [citiesAndCountries, setCitiesAndCountries] = useState<City[]>([]);
+  const [citiesAndCountries, setCitiesAndCountries] = useState<City[]>([]);
 
-    useEffect(() => {
-        fetch('https://countriesnow.space/api/v0.1/countries')
-            .then((response) => response.json())
-            .then((data) => {
-                const citiesAndCountriesData: City[] = data.data.reduce((acc: City[], country: any) => {
-                    if (Array.isArray(country.cities)) {
-                        const citiesWithCountry: City[] = country.cities.map((city: string) => ({
-                            label: `${city}, ${country.country}`,
-                            value: city,
-                            country: country.country,
-                        }));
-                        return acc.concat(citiesWithCountry);
-                    }
-                    return acc;
-                }, []);
+  useEffect(() => {
+    fetch('https://countriesnow.space/api/v0.1/countries')
+      .then((response) => response.json())
+      .then((data) => {
+        const citiesAndCountriesData: City[] = data.data.reduce((acc: City[], country: any) => {
+          if (Array.isArray(country.cities)) {
+            const citiesWithCountry: City[] = country.cities.map((city: string) => ({
+              label: `${city}, ${country.country}`,
+              value: city,
+              country: country.country,
+            }));
+            return acc.concat(citiesWithCountry);
+          }
+          return acc;
+        }, []);
 
-                setCitiesAndCountries(citiesAndCountriesData);
-            })
-            .catch((error) => {
-                console.error('Error fetching cities:', error);
-            });
-    }, []);
+        setCitiesAndCountries(citiesAndCountriesData);
+      })
+      .catch((error) => {
+        console.error('Error fetching cities:', error);
+      });
+  }, []);
 
-    const handleAutocompleteChange = (selectedCity: String | null) => {
-        if (selectedCity) {
-            citiesAndCountries.forEach((city: City) => {
-                if (city.value === selectedCity) {
-                    const { value: cityValue, country: cityCountry } = city;
-                    console.log(cityValue, cityCountry)
-                    // Perform the necessary navigation logic with the selected city and country
-                    window.location.href = `/destination/${cityCountry}/${cityValue}`;
-                }
-            });
+
+  /**
+  * Handles the change event of an autocomplete input, navigating to the selected city's page
+  * @param selectedCity The selected city from the autocomplete input
+  */
+  const handleAutocompleteChange = (selectedCity: String | null) => {
+    if (selectedCity) {
+      // Iterate through all cities and countries
+      citiesAndCountries.forEach((city: City) => {
+        // If the selected city matches a city in the array
+        if (city.value === selectedCity) {
+          // Destructure the city object to get the city value and country
+          const { value: cityValue, country: cityCountry } = city;
+          // Log the city value and country to the console
+          console.log(cityValue, cityCountry);
+          // Perform the necessary navigation logic with the selected city and country
+          window.location.href = `/destination/${cityCountry}/${cityValue}`;
         }
-    };
+      });
+    }
+  };
 
-    return (
-        <Autocomplete
-            className={styles.fixed}
-            label="Pick a city"
-            placeholder="Los Angeles"
-            data={citiesAndCountries}
-            onChange={handleAutocompleteChange}
-        />
-    )
+
+  return (
+    <Autocomplete
+      className={styles.fixed}
+      label="Pick a city"
+      placeholder="Los Angeles"
+      data={citiesAndCountries}
+      onChange={handleAutocompleteChange}
+    />
+  )
 }
