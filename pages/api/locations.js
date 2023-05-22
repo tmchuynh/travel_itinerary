f1225d5f4f8dd9e3456999ce378b509716d36b0c
tmchuynh <tinamchuynh@gmail.com>
2023-05-22 14:38:21 -0700

    update locations.js

--- pages/api/locations.js
+++ pages/api/locations.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       res.status(200).json(data);
     } catch (error) {
       console.error(error);
-      res.status(500).json({ error: 'An error occurred' });
+      res.status(500).json({ error: 'An error occurred BUT HEY I GOT HERE' });
     }
   } else {
     res.status(405).json({ error: 'Method Not Allowed' });
