314cd8d061ed325fb2154cb6ec10dab3b5ba8800
tmchuynh <tinamchuynh@gmail.com>
2023-05-22 10:32:59 -0700

    🎉 feat(MapComponent): replace OpenLayers with Google Maps API 🔧 chore(package.json): add google-maps-react dependency 🔧 chore(tsconfig.json): include MapComponent.jsx in the project The MapComponent has been updated to use the Google Maps API instead of OpenLayers. This change was made to improve the user experience and provide more features. The google-maps-react dependency was added to the project to enable the use of the Google Maps API. The tsconfig.json file was updated to include the MapComponent.jsx file in the project.

--- app/destination/[country]/[city]/components/MapComponent.jsx
+++ app/destination/[country]/[city]/components/MapComponent.jsx
@@ -1,35 +1,28 @@
-import { useEffect, useRef } from 'react';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
-import XYZ from 'ol/source/XYZ';
-import styles from './component.module.scss';
+// components/Map.js
+import React from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-const MapComponent = ({ center }) => {
-  const mapRef = useRef(null);
+const MapContainer = ({ google , position}) => {
+  const mapStyles = {
+    width: '100%',
+    height: '700px',
+  };
 
-  useEffect(() => {
-    const map = new Map({
-      target: mapRef.current,
-      layers: [
-        new TileLayer({
-          source: new XYZ({
-            url: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png'
-          })
-        })
-      ],
-      view: new View({
-        center: center || [33.8076787,-117.9731417],
-        zoom: 5
-      })
-    });
-
-    return () => {
-      map.setTarget(null);
-    };
-  }, [center]);
-
-  return <div ref={mapRef} className={styles.map} />;
+  return (
+    <Map
+      google={google}
+      zoom={14}
+      style={mapStyles}
+      initialCenter={{
+        lat: position[0],
+        lng: position[1],
+      }}
+    >
+      <Marker position={{ lat: position[0], lng: position[1] }} />
+    </Map>
+  );
 };
 
-export default MapComponent;
+export default GoogleApiWrapper({
+  apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+})(MapContainer);
