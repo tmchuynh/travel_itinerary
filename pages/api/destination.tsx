86dbe154b4537b1157c7b7c2de54fa897767c473
tmchuynh <tinamchuynh@gmail.com>
2023-05-24 11:04:27 -0700

    * This is an async function that handles a POST request to query a database for travel itinerary data  * based on a city and country input.

--- pages/api/destination.tsx
+++ pages/api/destination.tsx
@@ -6,6 +6,16 @@ interface DestinationRequestBody {
   cityValue: string;
 }
 
+/**
+ * This is an async function that handles a POST request to query a database for travel itinerary data
+ * based on a city and country input.
+ * @param {NextApiRequest} req - The `req` parameter is an object that represents the incoming HTTP
+ * request. It contains information about the request such as the HTTP method, headers, and body. It is
+ * of type `NextApiRequest`, which is a custom type defined by the Next.js framework.
+ * @param {NextApiResponse} res - `res` is an object representing the HTTP response that will be sent
+ * back to the client. It has methods like `status()` to set the HTTP status code, and `json()` to send
+ * a JSON response.
+ */
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     const { cityCountry, cityValue }: DestinationRequestBody = req.body;
